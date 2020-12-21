235bf720e6029f88799db995da30a1cd80629bdb
Cody Mullen <mullen.cody@yahoo.com>
2020-12-20 21:11:08 -0800

    fixing eslint issues and formatting

--- src/utils/Debounce.js
+++ src/utils/Debounce.js
@@ -1,23 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
-/*Custom Hook, creates a handler that grabs a value and only returns it after a certain amount of milliseconds.
-clearTimeout clears the handler every time the value changes to ensure debouncedValue is updated with correct value (it restarts setTimeout everytime value changes).*/
+
 function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
-    const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+    setDebouncedValue(value);
+    }, delay);
 
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebouncedValue(value);
-        }, delay);
+  return () => {
+    clearTimeout(handler);
+  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
 
-        return () => {
-            clearTimeout(handler);
-        };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [value]);
-
-    return debouncedValue;
+  return debouncedValue;
 };
 
 export default useDebounce;
