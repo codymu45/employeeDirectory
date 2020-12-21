e0311cb2634efebec827fbeeb2972f02b2c2cbcb
Cody Mullen <mullen.cody@yahoo.com>
2020-12-20 21:09:36 -0800

    adjusting formatting and comments

--- src/components/TableRow/index.js
+++ src/components/TableRow/index.js
@@ -1,19 +1,20 @@
+// Importing react and stylesheet
 import React from 'react';
 import './style.css';
 
 // Items for each user in their row
 function TableRow(props) {
-    return (
-        <tr>
-            <td>
-                <img alt={props.name} src={props.picture}></img>
-            </td>
-            <td>{props.name}</td>
-            <td>{props.phone}</td>
-            <td>{props.email}</td>
-            <td>{props.dob}</td>
-        </tr>
-    );
+  return (
+    <tr>
+      <td>
+        <img alt={props.name} src={props.picture}></img>
+      </td>
+      <td>{props.name}</td>
+      <td>{props.phone}</td>
+      <td>{props.email}</td>
+      <td>{props.dob}</td>
+    </tr>
+  );
 };
 
 export default TableRow;
